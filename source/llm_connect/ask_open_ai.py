e8dc5f78174166a5eae7e118f69cd46c6998370f
i17 <n.klievtsova@gmail.com>
2024-07-15 15:29:25 +0200

    add openai interface

---
+++ source/llm_connect/ask_open_ai.py
@@ -0,0 +1,84 @@
+from openai import OpenAI
+import json
+import os
+
+""" get openai key from .env """
+open_app = OpenAI(
+  api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+""" define prompt parametes """
+parameters = {
+    "temperature":0,
+    "max_tokens":1024,
+    "top_p": 1,
+    "frequency_penalty":0,
+    "presence_penalty":0
+}
+
+""" call gpt model via completions """
+def ask_gpt_complete(prompt,model,parameters):
+    try:
+        response = open_app.completions.create(
+            model=model,
+            prompt = prompt,
+            max_tokens=parameters["max_tokens"],
+            temperature = parameters["temperature"],
+            top_p = parameters["top_p"],
+            stop = ["ooooooooooo"]
+            )
+        return response.choices[0].text
+    except Exception as e:
+        return e
+
+""" call gpt model via chat completions - only user """
+def ask_gpt_chat(model,parameters,prompt):
+    try:
+        response = open_app.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "user", "content": prompt}
+            ],
+            temperature=parameters["temperature"],
+            max_tokens=parameters["max_tokens"],
+            top_p=parameters["top_p"],
+            frequency_penalty=parameters["frequency_penalty"],
+            presence_penalty=parameters["presence_penalty"]
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        return e
+
+""" call gpt model via chat completions - only user and system """
+def ask_gpt_system(model,parameters,prompt,system_message):
+    try:
+        response = open_app.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "user", "content": prompt},
+                {"role": "system", "content": system_message}
+            ],
+            temperature=parameters["temperature"],
+            max_tokens=parameters["max_tokens"],
+            top_p=parameters["top_p"],
+            frequency_penalty=parameters["frequency_penalty"],
+            presence_penalty=parameters["presence_penalty"]
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        return e
+
+""" call gpt model """
+def ask_gpt(model,prompt,system_message=0):
+    try:
+        if "instruct" in model:
+            response = ask_gpt_complete(prompt,model,parameters)
+        else:
+            if system_message == 0:
+                response = ask_gpt_chat(model,parameters,prompt)
+            else:
+                response = ask_gpt_system(model,parameters,prompt,system_message)
+        return response
+    except Exception as e:
+        return e
+
---
+++ test.py
@@ -0,0 +1,15 @@
+from source.llm_connect.ask_open_ai import ask_gpt
+
+# test = ask_gpt('gpt-3.5-turbo-instruct','do you know that bpmn is?')
+# print(test)
+
+# test = ask_gpt('gpt-3.5-turbo','do you know that bpmn is?')
+# print(test)
+
+# test = ask_gpt('gpt-4','do you know that bpmn is?')
+# print(test)
+
+# test = ask_gpt('gpt-4','do you know that bpmn is?','imagine please that you are a process modeler and an expert in this domain')
+# print(test)
+
+
