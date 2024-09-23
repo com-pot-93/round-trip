874d500e9c78183b3d4dd0b0af869d97a38864a3
Nataliia <n.klievtsova@gmail.com>
2024-09-23 13:04:59 +0200

    add new generated data with different tempreatures

--- src/general.py
+++ src/general.py
@@ -4,12 +4,16 @@ def create_directory(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-def check_metric(func, arg1, arg2, method=""):
+def check_metric(func, arg1, arg2, method="", similarity_threshold=""):
     try:
-        if method == "":
+        if method == "" and similarity_threshold == "":
             val = func(arg1, arg2)
-        else:
+        elif method != "" and similarity_threshold == "":
             val = func(arg1, arg2, method)
+        elif method == "" and similarity_threshold != "":
+            val = func(arg1, arg2, similarity_threshold = similarity_threshold)
+        else:
+            val = func(arg1, arg2, method, similarity_threshold)
     except:
         val = [0,0]
     return val
